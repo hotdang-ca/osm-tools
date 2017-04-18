b6f570c90d041cf7a4fc0500603dae813136bdfa
James Robert Perih <james@hotdang.ca>
2017-04-18 13:12:47 -0500

    Made the scripts more super

--- extract.sh
+++ extract.sh
@@ -2,17 +2,86 @@
 
 # (c) 2017 james robert perih
 
-FILE=$1
-LOCATION=$2
-AMENITY=$3
+FILE=
+LOCATION=
+AMENITY=
 
-echo 'Extracting amenity='$AMENITY' in '$LOCATION' from '$FILE'...'
+echo "Extract - amenity extraction tool for OpenStreetMap PBF files"
+echo "Part of osmtools"
+echo "(c) 2017 James Robert Perih"
+echo "james@hotdang.ca"
+echo ""
 
-osmosis -q --read-pbf $FILE --tf accept-ways amenity=$AMENITY --tf reject-relation --tf reject-nodes --used-node --write-xml output-ways-$LOCATION-$AMENITY.osm
-osmosis -q --read-pbf $FILE --tf accept-nodes amenity=$AMENITY --tf reject-relation --tf reject-ways --write-xml output-nodes-$LOCATION-$AMENITY.osm
-osmosis -q --read-xml ./output-ways-$LOCATION-$AMENITY.osm --read-xml ./output-nodes-$LOCATION-$AMENITY.osm --merge --write-xml $LOCATION-$AMENITY-all.osm
+function usage
+{
+    echo "usage: extract -f file -l location -a amenity | -h"
+    echo ""
+    echo "  -f <filename>: The filename of the .osm.pbf file"
+    echo "  -l <location>: The descriptive name of the Location."
+    echo "  -a <amenity_tag>: The tag of the amenity to extract."
+    echo ""
+    echo "  -h: this screen."
+    echo ""
+}
 
-rm ./output-ways-$LOCATION-$AMENITY.osm
-rm ./output-nodes-$LOCATION-$AMENITY.osm
+function run
+{
+  echo 'Extracting amenity='$AMENITY' in '$LOCATION' from '$FILE'...'
 
-echo 'Ready! Do parse.py' $LOCATION'-'$AMENITY'-all.osm'
+  osmosis -q --read-pbf $FILE --tf accept-ways amenity=$AMENITY --tf reject-relation --used-node --write-xml output-ways-$LOCATION-$AMENITY.osm
+  osmosis -q --read-pbf $FILE --tf accept-nodes amenity=$AMENITY --tf reject-relation --tf reject-ways --write-xml output-nodes-$LOCATION-$AMENITY.osm
+  osmosis -q --read-xml ./output-ways-$LOCATION-$AMENITY.osm --read-xml ./output-nodes-$LOCATION-$AMENITY.osm --merge --write-xml $LOCATION-$AMENITY-all.osm
+
+  rm ./output-ways-$LOCATION-$AMENITY.osm
+  rm ./output-nodes-$LOCATION-$AMENITY.osm
+  echo ''
+  echo 'Ready! Do parse.py' $LOCATION'-'$AMENITY'-all.osm'
+}
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -f | --file )           shift
+                                FILE=$1
+                                ;;
+        -l | --location )       shift
+                                LOCATION=$1
+                                ;;
+        -a | --amenity  )       shift
+                                AMENITY=$1
+                                ;;
+        -h | --help )           usage
+                                exit
+                                ;;
+        * )                     usage
+                                exit 1
+    esac
+    shift
+done
+
+# Validation
+error=0
+
+if [ "$FILE" = "" ]; then
+  echo "ERROR: You must specify file with -f <filename>"
+  error=1
+fi
+
+if [ "$LOCATION" = "" ]; then
+  echo "ERROR: You must specify a descriptive location with -l <location>"
+  error=1
+fi
+
+if [ "$AMENITY" = "" ]; then
+  echo "ERROR: You must specify an amenity tag to extract with -a <amenity_tag>"
+  error=1
+fi
+
+if [ "$error" = 1 ]; then
+  echo "Correct the parameters, and try again."
+  echo ""
+  usage
+  exit
+fi
+
+# still here? Let's run!
+run
--- parse.py
+++ parse.py
@@ -7,13 +7,22 @@ import sys
 import os
 import xml.etree.ElementTree
 
-if len(sys.argv) < 1:
-    print "Not enough arguments. Specify the filename."
+file_name = None
+place_filter = None
+
+if len(sys.argv) == 1:
+    print "Not enough arguments. Specify the filename, and optional filter."
+    print ""
+    print "Usage: parse.py <filename> \"[filter]\""
     os._exit(1)
 
-filename = sys.argv[1]
+if len(sys.argv) > 1:
+    file_name = sys.argv[1]
 
-e = xml.etree.ElementTree.parse(filename).getroot()
+if len(sys.argv) > 2:
+    place_filter = sys.argv[2]
+
+e = xml.etree.ElementTree.parse(file_name).getroot()
 
 for node in e.findall('node'):
     lat = node.get('lat')
@@ -22,15 +31,42 @@ for node in e.findall('node'):
     for nameNode in node.iter('tag'):
         if nameNode.get('k') == 'name':
             name = nameNode.get('v')
-            print("NODE: %s (%s, %s)" % (name, lat, lng) )
+
+            if place_filter != None:
+                if place_filter == name:
+                    print("%s,%s" % (lat, lng) )
+            else:
+                print("%s,%s)" % (lat, lng) )
 
 for way in e.findall('way'):
-    lat = way.get('lat')
-    lng = way.get('lon')
+    thisWayNodeList = []
+    thisWayName = ''
+    lat = ''
+    lng = ''
 
-    # print ("%s %s" % (lat, lng))
+    # Enumerate all Reference Nodes
+    for nodeRef in way.iter('nd'):
+        thisWayNodeList.append(nodeRef.get('ref'))
 
+    # Get all nodes with name
     for nameNode in way.iter('tag'):
         if nameNode.get('k') == 'name':
-            name = nameNode.get('v')
-            print("WAY: %s (%s, %s)" % (name, lat, lng) )
+            thisWayName = nameNode.get('v')
+
+            if place_filter != None:
+                if place_filter == thisWayName:
+                    # Get all reference nodes coordinates
+                    for node in e.findall('node'):
+                        if node.get('id') == thisWayNodeList[0]:
+                            lat = node.get('lat')
+                            lng = node.get('lon')
+
+                    print( "%s,%s" % (lat, lng) )
+            else:
+                # Get all reference nodes coordinates
+                for node in e.findall('node'):
+                    if node.get('id') == thisWayNodeList[0]:
+                        lat = node.get('lat')
+                        lng = node.get('lon')
+
+                print( "%s,%s" % (lat, lng) )
