8a05516da0e405c0dcef7abfee907efa14c06292
James Robert Perih <james@hotdang.ca>
2017-04-18 09:36:20 -0500

    output a little cleaner

--- update.sh
+++ update.sh
@@ -7,25 +7,31 @@ echo 'Updating OSM Data...'
 echo 'Downloading British Columbia Data...'
 curl http://download.geofabrik.de/north-america/canada/british-columbia-latest.osm.pbf --output pbf/british-columbia-latest.osm.pbf
 curl http://download.geofabrik.de/north-america/canada/british-columbia-latest.osm.pbf.md5 --output pbf/british-columbia-latest.osm.pbf.md5
+echo ''
 
 echo 'Downloading Alberta Data...'
 curl http://download.geofabrik.de/north-america/canada/alberta-latest.osm.pbf --output pbf/alberta-latest.osm.pbf
 curl http://download.geofabrik.de/north-america/canada/alberta-latest.osm.pbf.md5 --output pbf/alberta-latest.osm.pbf.md5
+echo ''
 
 echo 'Downloading Saskatchewan Data...'
 curl http://download.geofabrik.de/north-america/canada/saskatchewan-latest.osm.pbf --output pbf/saskatchewan-latest.osm.pbf
 curl http://download.geofabrik.de/north-america/canada/saskatchewan-latest.osm.pbf.md5 --output pbf/saskatchewan-latest.osm.pbf.md5
+echo ''
 
 echo 'Downloading Manitoba Data...'
 curl http://download.geofabrik.de/north-america/canada/manitoba-latest.osm.pbf --output pbf/manitoba-latest.osm.pbf
 curl http://download.geofabrik.de/north-america/canada/manitoba-latest.osm.pbf.md5 --output pbf/manitoba-latest.osm.pbf.md5
+echo ''
 
 echo 'Downloading Ontario Data...'
 curl http://download.geofabrik.de/north-america/canada/ontario-latest.osm.pbf --output pbf/ontario-latest.osm.pbf
 curl http://download.geofabrik.de/north-america/canada/ontario-latest.osm.pbf.md5 --output pbf/ontario-latest.osm.pbf.md5
+echo ''
 
 echo 'Downloading Quebec Data...'
 curl http://download.geofabrik.de/north-america/canada/quebec-latest.osm.pbf --output pbf/quebec-latest.osm.pbf
 curl http://download.geofabrik.de/north-america/canada/quebec-latest.osm.pbf.md5 --output pbf/quebec-latest.osm.pbf.md5
+echo ''
 
 echo 'Ready! Do extract.sh pbf/<filename>'
